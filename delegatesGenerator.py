cbba4b3788227ee8e2d3f9233e6e4af6e257e88e
mpolaczyk <marcin.polaczyk@gmail.com>
2014-11-01 03:12:48 +0100

    Delegate generator and snippet generator bugfixes.

--- delegatesGenerator.py
+++ delegatesGenerator.py
@@ -7,22 +7,23 @@ class Delegate:
 
     paramSuffix = ['', '_OneParam', '_TwoParams', '_ThreeParams', '_FourParams', '_FiveParams', '_SixParams', '_SevenParams', '_EightParams']
     
-    def __init__(self, kind, name, owningType, paramNames, paramTypes):
+    def __init__(self, kind, name, owningType, paramTypes, paramNames):
         self.kind = kind
         self.name = name
         self.owningType = owningType
-        self.paramNames = paramNames
         self.paramTypes = paramTypes
+        self.paramNames = paramNames
+        
 
     def Generate(self):
         a =  'DECLARE_${kind}'
         b = self.paramSuffix[len(self.paramNames)]
         c = '(${name}' if self.owningType == '' else ''.join(['({0}'.format(self.owningType), ', ${name}']) 
         d = ''
-        if len(self.paramTypes) > 0:
-            d = '' if len(self.paramNames) == 0 else (''.join([', {0}, {1}'.format(str(pn), str(pt)) for pn, pt in zip(self.paramNames, self.paramTypes)]))
+        if len(self.paramNames) > 0:
+            d = ''.join([', {0}, {1}'.format(str(pt), str(pn)) for pt, pn in zip(self.paramTypes, self.paramNames)])
         else:
-            d = '' if len(self.paramNames) == 0 else (''.join([', {0}'.format(str(pn)) for pn in self.paramNames]))
+            d = ''.join([', {0}'.format(str(pn)) for pn in self.paramTypes])
         e = ')'
         return Template(''.join([a,b,c,d,e])).substitute(kind = self.kind, name = self.name)
 
@@ -34,24 +35,24 @@ class DelegateGeneratorTest:
     
     def TestEvents(self):
         for n in range(1, self.maxParams + 2):
-            print(Delegate('EVENT', '$name$', '$type$', ['$$arg{0}$$'.format(p) for p in range(1,n)], []).Generate())
+            print(Delegate('EVENT', '$name$', '$owning_type$', ['$$type{0}$$'.format(p) for p in range(1,n)], []).Generate())
 
     
     def TestDelegates(self):
         for n in range(1, self.maxParams + 2):
-            print(Delegate('DELEGATE', '$name$', '', ['$$arg{0}$$'.format(p) for p in range(1,n)], []).Generate())
+            print(Delegate('DELEGATE', '$name$', '', ['$$type{0}$$'.format(p) for p in range(1,n)], []).Generate())
 
     
     def TestMulticastDeleates(self):
         for n in range(1, self.maxParams + 2):
-            print(Delegate('MULTICAST_DELEGATE', '$name$', '', ['$$arg{0}$$'.format(p) for p in range(1,n)], []).Generate())
+            print(Delegate('MULTICAST_DELEGATE', '$name$', '', ['$$type{0}$$'.format(p) for p in range(1,n)], []).Generate())
 
     
     def TestDynamicDelegates(self):
         for n in range(1, self.maxParams + 2):
-            print(Delegate('DYNAMIC_DELEGATE', '$name$', '', ['$$arg{0}$$'.format(p) for p in range(1,n)], ['$$type{0}$$'.format(p) for p in range(1,n)]).Generate())
+            print(Delegate('DYNAMIC_DELEGATE', '$name$', '', ['$$type{0}$$'.format(p) for p in range(1,n)], ['$$name{0}$$'.format(p) for p in range(1,n)]).Generate())
 
     
     def TestDynamicMulticastDelegates(self):
         for n in range(1, self.maxParams + 2):
-            print(Delegate('DYNAMIC_MULTICAST_DELEGATE', '$name$', '', ['$$arg{0}$$'.format(p) for p in range(1,n)], ['$$type{0}$$'.format(p) for p in range(1,n)]).Generate())
+            print(Delegate('DYNAMIC_MULTICAST_DELEGATE', '$name$', '', ['$$type{0}$$'.format(p) for p in range(1,n)], ['$$name{0}$$'.format(p) for p in range(1,n)]).Generate())
--- snippetsGenerator.py
+++ snippetsGenerator.py
@@ -2,45 +2,44 @@
 
 from delegatesGenerator import Delegate
 
-
 class DelegateSnippet:
 
-    def __init__(self, title, author, description, shortcut, delegateKind, delegateOwningType, delegateParamNames, delegateParamTypes):
+    def __init__(self, title, author, description, shortcut, delegateKind, delegateOwningType, delegateParamTypes, delegateParamNames):
         self.title = title
         self.author = author
         self.description = description
         self.shortcut = shortcut
         self.delegateKind = delegateKind
         self.delegateOwningType = delegateOwningType
-        self.delegateParamNames = delegateParamNames
         self.delegateParamTypes = delegateParamTypes
+        self.delegateParamNames = delegateParamNames
 
     def Generate(self):
         a = '<?xml version=\"1.0\" encoding=\"utf-8\"?>\n\
-        <CodeSnippets xmlns=\"http://schemas.microsoft.com/VisualStudio/2005/CodeSnippet\">\n\
-          <CodeSnippet Format=\"1.0.0\">\n\
-            <Header>\n\
-              <SnippetTypes>\n\
-                <SnippetType>Expansion</SnippetType>\n\
-              </SnippetTypes>\n\
-              <Title>{title}</Title>\n\
-              <Author>{author}</Author>\n\
-              <Description>{description}</Description>\n\
-              <HelpUrl>\n\
-              </HelpUrl>\n\
-              <Shortcut>{shortcut}</Shortcut>\n\
-            </Header>\n\
-            <Snippet>\n\
-              <Declarations>\n'.format(title=self.title, author=self.author, description=self.description, shortcut=self.shortcut)	  
-        b = '<Literal Editable=\"true\">\n\
-          <ID>$delegateName$</ID>\n\
+    <CodeSnippets xmlns=\"http://schemas.microsoft.com/VisualStudio/2005/CodeSnippet\">\n\
+      <CodeSnippet Format=\"1.0.0\">\n\
+        <Header>\n\
+          <SnippetTypes>\n\
+            <SnippetType>Expansion</SnippetType>\n\
+          </SnippetTypes>\n\
+          <Title>{title}</Title>\n\
+          <Author>{author}</Author>\n\
+          <Description>{description}</Description>\n\
+          <HelpUrl>\n\
+          </HelpUrl>\n\
+          <Shortcut>{shortcut}</Shortcut>\n\
+        </Header>\n\
+        <Snippet>\n\
+          <Declarations>\n'.format(title=self.title, author=self.author, description=self.description, shortcut=self.shortcut)	  
+        b = '       <Literal Editable=\"true\">\n\
+          <ID>delegateName</ID>\n\
           <ToolTip>Delegate type name</ToolTip>\n\
           <Default>DelegateName</Default>\n\
           <Function>\n\
           </Function>\n\
         </Literal>\n'
-        c = '<Literal Editable=\"true\">\n\
-          <ID>$owningType$</ID>\n\
+        c = '' if self.delegateOwningType == '' else '       <Literal Editable=\"true\">\n\
+          <ID>owningType</ID>\n\
           <ToolTip>Delegate owning type name</ToolTip>\n\
           <Default>OwningType</Default>\n\
           <Function>\n\
@@ -48,40 +47,27 @@ class DelegateSnippet:
         </Literal>\n'
         d = ''
         for p in range(1, self.delegateParamNames+1):
-            d += '<Literal Editable=\"true\">\n\
-              <ID>$paramName{0}$</ID>\n\
-              <ToolTip>Parameter name</ToolTip>\n\
-              <Default>Param{1}Name</Default>\n\
-              <Function>\n\
-              </Function>\n\
+            d += '        <Literal Editable=\"true\">\n\
+          <ID>paramName{0}</ID>\n\
+          <ToolTip>Parameter name</ToolTip>\n\
+          <Default>Param{1}Name</Default>\n\
+          <Function>\n\
+          </Function>\n\
             </Literal>\n'.format(p,p)
         e = ''
         for p in range(1, self.delegateParamTypes+1):
-            e += '<Literal Editable=\"true\">\n\
-              <ID>$paramType{0}$</ID>\n\
-              <ToolTip>Parameter type</ToolTip>\n\
-              <Default>Param{1}type</Default>\n\
-              <Function>\n\
-              </Function>\n\
+            e += '       <Literal Editable=\"true\">\n\
+          <ID>paramType{0}</ID>\n\
+          <ToolTip>Parameter type</ToolTip>\n\
+          <Default>Param{1}Type</Default>\n\
+          <Function>\n\
+          </Function>\n\
             </Literal>\n'.format(p,p)
-        f = '</Declarations>\n\
-              <Code Language=\"cpp\"><![CDATA['
-        g = Delegate(self.delegateKind, '$delegateName$', self.delegateOwningType, ['$$paramName{0}$$'.format(p) for p in range(1,self.delegateParamNames+1)], ['$$paramType{0}$$'.format(p) for p in range(1,self.delegateParamTypes+1)]).Generate()
+        f = '        </Declarations>\n\
+          <Code Language=\"cpp\"><![CDATA['
+        g = Delegate(self.delegateKind, '$delegateName$', self.delegateOwningType, ['$$paramType{0}$$'.format(p) for p in range(1,self.delegateParamTypes+1)], ['$$paramName{0}$$'.format(p) for p in range(1,self.delegateParamNames+1)]).Generate()
         h = '$selected$ $end$]]></Code>\n\
-            </Snippet>\n\
-          </CodeSnippet>\n\
-        </CodeSnippets>'        
-        out = [a, b, c, d, e, f, g, h]
-        return ''.join(out)
-
-
-
-
-
-
-
-
-
-
-
-
+        </Snippet>\n\
+      </CodeSnippet>\n\
+    </CodeSnippets>'        
+        return ''.join([a, b, c, d, e, f, g, h])
