e17f38b746b70722ef97f06bfa976c2f93f53fb8
mpolaczyk <marcin.polaczyk@gmail.com>
2014-11-01 02:25:24 +0100

    Event generation corrected. Minor changes.

--- delegatesGenerator.py
+++ delegatesGenerator.py
@@ -7,23 +7,24 @@ class Delegate:
 
     paramSuffix = ['', '_OneParam', '_TwoParams', '_ThreeParams', '_FourParams', '_FiveParams', '_SixParams', '_SevenParams', '_EightParams']
     
-    def __init__(self, kind, name, paramNames, paramTypes):
+    def __init__(self, kind, name, owningType, paramNames, paramTypes):
         self.kind = kind
         self.name = name
+        self.owningType = owningType
         self.paramNames = paramNames
         self.paramTypes = paramTypes
 
     def Generate(self):
         a =  'DECLARE_${kind}'
         b = self.paramSuffix[len(self.paramNames)]
-        c = '(${name}'
+        c = '(${name}' if self.owningType == '' else ''.join(['({0}'.format(self.owningType), ', ${name}']) 
         d = ''
         if len(self.paramTypes) > 0:
             d = '' if len(self.paramNames) == 0 else (''.join([', {0}, {1}'.format(str(pn), str(pt)) for pn, pt in zip(self.paramNames, self.paramTypes)]))
         else:
             d = '' if len(self.paramNames) == 0 else (''.join([', {0}'.format(str(pn)) for pn in self.paramNames]))
         e = ')'
-        print(Template('%s%s%s%s%s' % (a,b,c,d,e)).substitute(kind = self.kind, name = self.name))
+        return Template(''.join([a,b,c,d,e])).substitute(kind = self.kind, name = self.name)
 
 
 class DelegateGeneratorTest:
@@ -33,24 +34,24 @@ class DelegateGeneratorTest:
     
     def TestEvents(self):
         for n in range(1, self.maxParams + 2):
-            Delegate('EVENT', '$name$', ['$$arg{0}$$'.format(p) for p in range(1,n)], []).Generate()
+            print(Delegate('EVENT', '$name$', '$type$', ['$$arg{0}$$'.format(p) for p in range(1,n)], []).Generate())
 
     
     def TestDelegates(self):
         for n in range(1, self.maxParams + 2):
-            Delegate('DELEGATE', '$name$', ['$$arg{0}$$'.format(p) for p in range(1,n)], []).Generate()
+            print(Delegate('DELEGATE', '$name$', '', ['$$arg{0}$$'.format(p) for p in range(1,n)], []).Generate())
 
     
     def TestMulticastDeleates(self):
         for n in range(1, self.maxParams + 2):
-            Delegate('MULTICAST_DELEGATE', '$name$', ['$$arg{0}$$'.format(p) for p in range(1,n)], []).Generate()
+            print(Delegate('MULTICAST_DELEGATE', '$name$', '', ['$$arg{0}$$'.format(p) for p in range(1,n)], []).Generate())
 
     
     def TestDynamicDelegates(self):
         for n in range(1, self.maxParams + 2):
-            Delegate('DYNAMIC_DELEGATE', '$name$', ['$$arg{0}$$'.format(p) for p in range(1,n)], ['$$type{0}$$'.format(p) for p in range(1,n)]).Generate()
+            print(Delegate('DYNAMIC_DELEGATE', '$name$', '', ['$$arg{0}$$'.format(p) for p in range(1,n)], ['$$type{0}$$'.format(p) for p in range(1,n)]).Generate())
 
     
     def TestDynamicMulticastDelegates(self):
         for n in range(1, self.maxParams + 2):
-            Delegate('DYNAMIC_MULTICAST_DELEGATE', '$name$', ['$$arg{0}$$'.format(p) for p in range(1,n)], ['$$type{0}$$'.format(p) for p in range(1,n)]).Generate()
+            print(Delegate('DYNAMIC_MULTICAST_DELEGATE', '$name$', '', ['$$arg{0}$$'.format(p) for p in range(1,n)], ['$$type{0}$$'.format(p) for p in range(1,n)]).Generate())
